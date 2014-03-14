3cfb6ce9e79d13174e6ecef0ca5e05d8f080e44a
Juraj Petrik <juraj.petrik@taqtile.com>
2014-03-14 00:19:25 -0300

    refactored shit

--- pony_mover.js
+++ pony_mover.js
@@ -1,9 +1,123 @@
-function PonyMover($pony, $log) {
-  this.$pony = $pony
-  this.$log = $log
 
-  // Stack of all the moves. Eg.: ['left', 'up', 'down']
-  this.moves = []
+
+
+/* The Invoker function */
+var Switch = function(){
+    var _commands = [];
+    this.$log = $log;
+    this.storeAndExecute = function(command){
+        _commands.push(command);
+        command.execute();
+        this.$log.append('<li>' + command.name() + '</li>')
+    }
+
+    this.undoLast = function()
+    {
+      var lastCommand = _commands.pop;
+      lastCommand.prototype.undo();
+      this.$log.find('li:last').remove()
+    }
+}
+
+/*The Commands*/
+
+var commandUp = function(receiver)
+{
+    this.execute = function()
+    {
+      receiver.moveUp();
+    }
+
+    this.undo = function()
+    {
+      receiver.moveDown();
+    }
+
+    this.name = function()
+    {
+      return "Up";
+    }
+}
+
+var commandDown = function(receiver)
+{
+    this.execute = function()
+    {
+      receiver.moveDown();
+    }
+
+    this.undo = function()
+    {
+      receiver.moveUp();
+    }
+
+    this.name = function()
+    {
+      return "Down";
+    }
+}
+
+var commandLeft = function(receiver)
+{
+    this.execute = function()
+    {
+      receiver.moveLeft();
+    }
+
+    this.undo = function()
+    {
+      receiver.moveRight();
+    }
+
+    this.name = function()
+    {
+      return "Left";
+    }
+}
+
+var commandRight = function(receiver)
+{
+    this.execute = function()
+    {
+      receiver.moveRight();
+    }
+
+    this.undo = function()
+    {
+      receiver.moveLeft();
+    }
+
+
+    this.name = function()
+    {
+      return "Right";
+    }
+}
+
+/* The Receiver is .... Pony*/
+var Pony = function(){
+    var pony = $('#pony');
+
+    function moveUp()
+    {
+       receiver.animate({'top': '-=30px'});
+    }
+
+    function moveDown()
+    {
+      receiver.animate({'top': '+=30px'});
+    }
+
+    function moveLeft()
+    {
+      receiver.animate({'left': '-=30px'});
+
+    }
+
+    function moveRight()
+    {
+       receiver.animate({'top': '+=30px'});
+    }
 }
 
 PonyMover.prototype.moveDirection = function(direction) {
@@ -20,34 +134,5 @@ PonyMover.prototype.moveDirection = function(direction) {
     case 'right':
       this.$pony.animate({'left': '+=30px'})
       break
-  }
-}
-
-PonyMover.prototype.move = function(keyCode) {
-  var direction = keyCodeToName[keyCode] // Convert key code to direction name
-
-  if (direction) {
-    this.moveDirection(direction)
-    this.moves.push(direction)
-    this.$log.append('<li>' + direction + '</li>')
-  }
-}
-
-PonyMover.prototype.oppositeDirections = {
-  'up': 'down',
-  'down': 'up',
-  'left': 'right',
-  'right': 'left'
-}
-
-PonyMover.prototype.undo = function() {
-  // Get the last move
-  var direction = this.moves.pop()
-
-  if (direction) {
-    var oppositeDirection = this.oppositeDirections[direction]
-
-    this.moveDirection(oppositeDirection)
-    this.$log.find('li:last').remove()
   }
 }
